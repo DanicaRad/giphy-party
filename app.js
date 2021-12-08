278b093d06fa3b171d0e2a06c2ab4cc16f6c4078
DanicaRad <87402525+DanicaRad@users.noreply.github.com>
2021-12-07 17:55:07 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+async function randomGiphy() {
+    const res = await axios.get('http://api.giphy.com/v1/gifs/random', {params:{api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym', limit: 1}});
+    console.log('res', res);
+    const gif = res.data.data.images.fixed_height;
+    console.log('gif', gif);
+    appendGiphy(gif.url);
+}
+
+async function searchGiphy(q) {
+    const res = await axios.get('http://api.giphy.com/v1/gifs/search', {params: new Params(q)});
+    console.log(res);
+    const gif = res.data.data.images.fixed_height;
+    appendGiphy(gif.url);
+}
+
+class Params {
+    constructor(q) {
+        this.api_key = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+        this.limit = 1;
+        this.q = q;
+    }
+}
+
+const flexBox = document.querySelector('#gifs');
+const searchBtn = document.querySelector('#get-gif');
+const qTerm = document.getElementById('search');
+
+function appendGiphy(gif) {
+    const newImg = document.createElement('img');
+    newImg.src = gif;
+    console.log(flexBox);
+    flexBox.append(newImg);
+}
+
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const qTerm = document.getElementById('search');
+    if(qTerm.value.length >= 1) {
+        searchGiphy(qTerm.value);
+    }
+    randomGiphy();    
+})
+
+const removeBtn = document.getElementById('remove');
+removeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    flexBox.innerHTML = '';
+})
+
+
+
